8a6e7875ee42d87e9f07f18d440585d7de50458f
Erik Nguita <erik.nguita@fuib.com>
2023-01-12 16:23:53 +0700

    added csv examples

--- csv/src/index.ts
+++ csv/src/index.ts
@@ -16,17 +16,38 @@ import * as fs from 'fs';
 // })()
 
 
-// -- Reading by batch and streaming to csv
+// // -- Reading by batch and streaming to csv
+// (async () => {
+//   const data = Array(100).fill({ header1: "data1", header2: "data2" })
+//
+//   const stringifier = csv.stringify({
+//     columns: ['header1', 'header2'],
+//     header: true
+//   });
+//
+//   data.map((row) => {
+//     stringifier.write([row.header1, row.header2]);
+//   })
+//
+//
+//   stringifier.end();
+//   const dest = fs.createWriteStream('files/csv-test.csv', { encoding: "utf-8" })
+//
+//   stringifier.pipe(dest)
+// })()
+
+
+// -- custom headers
 (async () => {
-  const data = Array(100).fill({ header1: "data1", header2: "data2" })
+  const data = Array(10).fill({ header1: "data1", header2: "data2" })
 
   const stringifier = csv.stringify({
-    columns: ['header1', 'header2'],
+    columns: {header1: "Заголовок 1", header2: "Заголовок 2"},
     header: true
   });
 
   data.map((row) => {
-    stringifier.write([row.header1, row.header2]);
+    stringifier.write(row);
   })
 
 
