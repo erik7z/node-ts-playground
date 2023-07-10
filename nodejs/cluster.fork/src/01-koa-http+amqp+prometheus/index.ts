1753584d1968abc706828635abb4cc056206c0f1
Erik Nguita <e.nhuita@betinvest.com>
2023-07-10 11:01:19 +0700

    implemented prometheus metrics for master and workers in cluster;

--- nodejs/cluster.fork/src/01-koa-http+amqp+prometheus/index.ts
+++ nodejs/cluster.fork/src/01-koa-http+amqp+prometheus/index.ts
@@ -2,21 +2,35 @@ import cluster from 'cluster';
 import os from 'os';
 import Koa from 'koa';
 import Router from 'koa-router';
-import amqp, { Channel } from 'amqplib';
-import { koaMiddleware } from 'prometheus-api-metrics'
+import amqp, {Channel} from 'amqplib';
+import {koaMiddleware} from 'prometheus-api-metrics'
+import Prometheus from 'prom-client';
 
+Prometheus.register.setDefaultLabels({
+  process: process.pid.toString(),
+    role: cluster.isMaster ? 'master' : 'worker'
+})
 
-const app = new Koa();
+async function startServer(role: string, router: Router) {
+    const app = new Koa();
 
-app.use(koaMiddleware({
-    metricsPath: '/metrics',
-    additionalLabels: ['role'],
-    extractAdditionalLabelValuesFn: () => ({ role: cluster.isMaster ? 'master' : 'worker' }),
-}))
+    app.use(koaMiddleware({
+        metricsPath: '/metrics',
+    }));
+
+    app.use(router.routes()).use(router.allowedMethods());
+
+    app.listen(3000, () => {
+        console.log(`${role} process ${process.pid} is listening on port 3000`);
+    });
+}
 
 const router = new Router();
 
 if (cluster.isMaster) {
+    // Object to store worker metrics
+    const workerMetrics = new Map<number, any>();
+
     console.log(`Master process ${process.pid} is running`);
 
     router.get('/', async function metrics (ctx: any, next: any)  {
@@ -27,16 +41,26 @@ if (cluster.isMaster) {
         ctx.body = `Master process ${process.pid} is running`;
     });
 
-    app.use(router.routes()).use(router.allowedMethods());
+    router.get('/metrics', async (ctx: any) => {
+        const mainMetrics = await Prometheus.register.getMetricsAsJSON()
+        const newMetrics = Prometheus.AggregatorRegistry.aggregate([mainMetrics, ...workerMetrics.values()])
 
-    app.listen(3000, () => {
-        console.log(`Master process ${process.pid} is listening on port 3000`);
+        ctx.body = await newMetrics.metrics()
     });
+
+    startServer('Master', router);
 
     // Fork workers
     const numCPUs = os.cpus().length;
     for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+        const worker = cluster.fork();
+
+        worker.on('message', async (msg) => {
+            if(msg.metrics && msg.metrics.length) {
+                console.log('Master process received metrics from worker', msg.metrics.length)
+                workerMetrics.set(worker.id, msg.metrics)
+            }
+        })
     }
 
     cluster.on('exit', (worker, code, signal) => {
@@ -45,8 +69,23 @@ if (cluster.isMaster) {
         cluster.fork();
     });
 } else {
-    // Worker process
     console.log(`Worker process ${process.pid} started`);
+
+    Prometheus.collectDefaultMetrics({register: Prometheus.register})
+
+    const amqpMsgCount = new Prometheus.Counter({
+        labelNames: ['type', 'endpoint'],
+        name: 'amqp_incoming_messages_total',
+        help: 'The total number of incoming AMQP messages',
+        registers: [Prometheus.register]
+    });
+
+    setInterval(async () => {
+        const metrics = await Prometheus.register.getMetricsAsJSON()
+        if(process.send) {
+            process.send({metrics});
+        }
+    }, 5000)
 
     const consumeMessages = async (): Promise<void> => {
         try {
@@ -61,6 +100,7 @@ if (cluster.isMaster) {
                 if (msg !== null) {
                     console.log(`Worker process ${process.pid} received message: ${msg.content.toString()}`);
                     channel.ack(msg);
+                    amqpMsgCount.labels('amqp', 'someAmqpEnpoint').inc();
                 }
             });
         } catch (error) {
